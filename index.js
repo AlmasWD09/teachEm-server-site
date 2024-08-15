b505bd93406e8af867fc5726039ec88bae0cd1b0
almas hossain <almashossain7384@gmail.com>
2024-08-16 00:26:08 +0600

    jwt token generate

--- index.js
+++ index.js
@@ -49,7 +49,12 @@ async function run() {
     app.post('/jwt', async (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '365d' });
-      res.send({ token });
+      res.cookie('token',token,{
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      })
+      .send({ success: true })
     })
 
     // middlewares 
